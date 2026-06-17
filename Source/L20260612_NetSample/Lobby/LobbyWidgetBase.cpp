d9c15cc469319daa510327305d4e01f0647a272e
junios-study <junios@naver.com>
2026-06-17 11:26:29 +0900

    위젯 제어코드 만들기

--- Source/L20260612_NetSample/Lobby/LobbyWidgetBase.cpp
+++ Source/L20260612_NetSample/Lobby/LobbyWidgetBase.cpp
@@ -2,4 +2,51 @@
 
 
 #include "LobbyWidgetBase.h"
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Components/TextBlock.h"
+#include "Components/ScrollBox.h"
+
+void ULobbyWidgetBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	StartButton->OnClicked.AddDynamic(this, &ULobbyWidgetBase::PressStart);
+	if (SendButton)
+	{
+		SendButton->OnClicked.AddDynamic(this, &ULobbyWidgetBase::PressSend);
+	}
+
+	InputText->OnTextCommitted.AddDynamic(this, &ULobbyWidgetBase::PressChatTextEnder);
+	InputText->OnTextChanged.AddDynamic(this, &ULobbyWidgetBase::ProcessTextChange);
+	
+
+}
+
+
+void ULobbyWidgetBase::PressStart()
+{
+
+}
+
+void ULobbyWidgetBase::PressSend()
+{
+}
+
+void ULobbyWidgetBase::PressChatTextEnder(const FText& Text, ETextCommit::Type CommitMethod)
+{
+}
+
+void ULobbyWidgetBase::ProcessTextChange(const FText& Text)
+{
+}
+
+void ULobbyWidgetBase::UpdateLeftTime(const int32 InLeftTime)
+{
+}
+
+void ULobbyWidgetBase::UpdateConnectionCount(const int32 InConnectionCount)
+{
+}
+
 
